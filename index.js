3e66d67d9694b9baf80ccb5ed666a458199b6077
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-09-17 11:02:58 -0500

    completed Task 2, Request 4

--- index.js
+++ index.js
@@ -147,9 +147,12 @@ function lowPopulationAnimals(animals) {
   Remember the reduce method takes two arguments: a callback (which itself takes two args - the accumulator and the item), and an initial value for the count.
   */
 
-function USApop(/*Your Code Here*/) {
-  /*Your Code Here*/
+function USApop(animals) {
+  return animals.reduce((population, animal) => {
+    return population + animal.population
+  }, 0)
 }
+// console.log('USApop:', USApop(zooAnimals))
 
 // 🦁🦁🦁 Callbacks 🦁🦁🦁
 /* 🦁🦁🦁 Step 1: Create a higher-order function 🦁🦁🦁
